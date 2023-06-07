5c4b2b433c50a59580a33ebc8b52514c0d2d774d
Sam <samuel.knd.9@gmail.com>
2023-06-06 20:55:44 -0600

    Version 1

---
+++ src/Poker/Card.java
@@ -0,0 +1,36 @@
+public class Card {
+    private String palo, color, valor;
+    public Card(String palo, String color, String valor){
+        this.setPalo(palo);
+        this.setColor(color);
+        this.setValor(valor);
+    }
+
+    public String getCard(){
+        return getPalo() + " " + getColor() + " " + getValor();
+    }
+
+    public void setPalo(String palo) {
+        this.palo = palo;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getPalo() {
+        return palo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+}
